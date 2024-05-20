767a7323785637cbb05fda246460fc8809a97757
wodecinghaolen <yotsuba8629@outlook.com>
2024-05-20 10:34:49 -0500

    Update versionControlDialog.ts

--- versionControlDialog.ts
+++ versionControlDialog.ts
@@ -1,12 +1,13 @@
-import { LitElement, html, css, property } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { LitElement, html, css } from 'lit';
+import { customElement, property, state } from 'lit/decorators.js';
 import '@material/mwc-dialog';
 
-@customElement('text-version-selector')
+@customElement('version-control-dialog')
 export class TextVersionSelector extends LitElement {
   @property({ type: Array }) listOfTextVersions: string[] = [];
   @property({ type: String }) selectedVersion: string = '';
   @property({ type: String }) selectedText: string = '';
+  @property({ type: Boolean }) showDialog: boolean = false;
 
   static styles = css`
     /* Add your component styles here */
@@ -14,15 +15,25 @@ export class TextVersionSelector extends LitElement {
 
   render() {
     return html`
-      <mwc-dialog heading="Select Text Version">
+      <mwc-dialog .open="${this.showDialog}" @closed="${this.handleDialogClose}" heading="Select Text Version">
         <select @change="${this.handleVersionChange}">
           ${this.listOfTextVersions.map(version => html`
             <option value="${version}">${version}</option>
           `)}
         </select>
         <textarea .value="${this.selectedText}" readonly></textarea>
+        <mwc-button slot="primaryAction" @click="${this.handleOk}">OK</mwc-button>
+        <mwc-button slot="secondaryAction" @click="${this.handleCancel}">Cancel</mwc-button>
       </mwc-dialog>
     `;
+  }
+
+  openDialog() {
+    this.showDialog = true;
+  }
+
+  handleDialogClose() {
+    this.showDialog = false;
   }
 
   handleVersionChange(event: Event) {
@@ -30,6 +41,20 @@ export class TextVersionSelector extends LitElement {
     this.selectedVersion = selectElement.value;
     // Assuming you have a function to get the text based on the selected version
     this.selectedText = this.getTextForVersion(this.selectedVersion);
+  }
+
+  handleOk() {
+    // Handle OK button click, for example, submit the selected version
+    console.log('OK clicked');
+    // Close the dialog
+    this.showDialog = false;
+  }
+
+  handleCancel() {
+    // Handle Cancel button click, for example, discard changes
+    console.log('Cancel clicked');
+    // Close the dialog
+    this.showDialog = false;
   }
 
   getTextForVersion(version: string): string {
